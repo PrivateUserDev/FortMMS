230efad1906f481c651cb5226c9ad4582e77ee3d
PrivateUser <kjswennen@gmail.com>
2025-06-14 16:50:46 +0200

    add config for yap

--- config/config.js
+++ config/config.js
@@ -1,13 +1,17 @@
 const config = {
-  port: process.env.PORT || 80,
-  host: process.env.HOST || '0.0.0.0',
+  port: process.env.PORT || 80, // port the server will run on
+  host: process.env.HOST || '0.0.0.0', // to forward this with cloudflare, create a Zero Trust tunnel and get it from http://127.0.0.1:80
   wsMaxPayload: 1048576,
 
   matchmakingTimeout: 30000,
-  maxQueueSize: 100,
+  maxQueueSize: 100, // max amount of people in one queue
   bannedIPs: new Set([
     'ip here',
-  ])
+  ]),
+
+  bEnableGameSessions: false, // enables gamesessions, you need additional code for this in your gameserver.
+  bEnableQueues: false, // enables queue's
+  bEnableBackfil: false, // let's people join a server until the match started (instant queues)
 };
 
 module.exports = config;
